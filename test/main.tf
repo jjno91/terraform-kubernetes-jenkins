7a576e7895db8ececf6debc4ed4a1a063a39520f
Jeremiah Norris <jjno91@gmail.com>
2020-02-21 16:36:34 -0600

    add missing test parameter

--- test/main.tf
+++ test/main.tf
@@ -2,8 +2,9 @@ module "this" {
   source = "../"
 
   # terraform plan should work with fake values here
-  jenkins_volume_id   = "abc-123"
-  jenkins_volume_size = "100"
-  certificate_arn     = "this"
-  host                = "this.com"
+  jenkins_volume_id        = "abc-123"
+  jenkins_volume_size      = "100"
+  volume_availability_zone = "this"
+  certificate_arn          = "this"
+  host                     = "this.com"
 }
