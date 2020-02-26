2228016b0932d0b75a1928b4fd3f3f2d06ded911
Jeremiah Norris <jjno91@gmail.com>
2020-02-25 23:06:50 -0600

    remove cert from unit test

--- test/main.tf
+++ test/main.tf
@@ -5,6 +5,5 @@ module "this" {
   jenkins_volume_id        = "abc-123"
   jenkins_volume_size      = "100"
   volume_availability_zone = "this"
-  certificate_arn          = "this"
   host                     = "this.com"
 }
